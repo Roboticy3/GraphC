42a078b7f7c0e265455734bed37fcafb4f91bbec
Pablo Ibarz <fribarz123@gmail.com>
2024-10-29 23:31:39 -0400

    removed builds

--- build-unix/CMakeFiles/GraphC.dir/src/main.c.o.d
+++
@@ -1,7 +0,0 @@
-CMakeFiles/GraphC.dir/src/main.c.o: \
- /home/pablo/lang-experiments/GraphC/src/main.c \
- /usr/include/stdc-predef.h \
- /home/pablo/lang-experiments/GraphC/include/programs/randomgraph.h \
- /home/pablo/lang-experiments/GraphC/include/programs/petersen.h \
- /home/pablo/lang-experiments/GraphC/include/programs/maxmatching.h \
- /home/pablo/lang-experiments/GraphC/include/programs/queuetest.h
