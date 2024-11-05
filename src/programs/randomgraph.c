28522c2468c26c3473a3551c264eee2f0a60dd85
Pablo Ibarz <fribarz123@gmail.com>
2024-11-04 22:05:51 -0500

    removed extra stuff

--- src/programs/randomgraph.c
+++ src/programs/randomgraph.c
@@ -27,27 +27,7 @@ int randomgraph() {
     printf("%f, ", p);
     fill_graph_random(&g, p);
     
-    //print_graph(g);
-
-    size_t shortest_paths[G_ORDER] = { [ 0 ... G_ORDER - 1] = -1};
-    PairEdge shortest_paths_cycles[G_MAX_EDGES] = {0};
-    ShortestPathGraph t = {shortest_paths, G_ORDER, 0, shortest_paths_cycles, 0};
-
-    bfs(g, &t);
-
-    size_t cycle_lengths_block[G_MAX_EDGES];
-    array cycle_lengths_array = {cycle_lengths_block, 0, G_MAX_EDGES};
-
-    cycle_lengths(t, &cycle_lengths_array);
-
-    //printf("found %ld cycles\n", cycle_lengths_array.length);
-
-    for (size_t i = 0; i < cycle_lengths_array.length; i++) {
-      printf("%ld, ", cycle_lengths_block[i]);
-    }
-    printf("\n");
-
-    p += 0.01;
+    print_graph(g);
   }
 
   
